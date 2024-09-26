5bf6ec3ced606983e4a2251bf0f9fbb95e28d901
kburke9 <125512965+kburke9@users.noreply.github.com>
2024-09-26 14:57:34 +0000

    Exercise

--- tour-service/src/main/java/com/example/tourservice/TravelAgentService.java
+++ tour-service/src/main/java/com/example/tourservice/TravelAgentService.java
@@ -1,6 +1,9 @@
 package com.example.tourservice;
 
+import org.springframework.stereotype.Service;
+
 import com.example.tourservice.utilities.TourRepository;
+@Service
 
 public class TravelAgentService {
     private  TourRepository tourRepository;
